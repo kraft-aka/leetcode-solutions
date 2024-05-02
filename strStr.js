8b539eaa78230d5c9cde7321992840c17d3dee11
kraft-aka <kubat.aiupov@gmail.com>
2024-05-02 22:16:19 +0200

    add strStr

---
+++ strStr.js
@@ -0,0 +1,12 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+// or -1 if needle is not part of haystack.
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+var strStr = function (haystack, needle) {
+  return haystack.includes(needle) ? haystack.indexOf(needle) : -1;
+};
