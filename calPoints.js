1eccb81e1ea43badb32e05677267dbd02666e554
kubat <kubat.aiupov@gmail.com>
2024-06-19 23:13:37 +0200

    add calPoints

---
+++ calPoints.js
@@ -0,0 +1,54 @@
+// You are keeping the scores for a baseball game with strange rules. At the
+// beginning of the game, you start with an empty record.
+
+// You are given a list of strings operations, where operations[i] is the ith
+// operation you must apply to the record and is one of the following:
+
+// An integer x.
+// Record a new score of x.
+// '+'.
+// Record a new score that is the sum of the previous two scores.
+// 'D'.
+// Record a new score that is the double of the previous score.
+// 'C'.
+// Invalidate the previous score, removing it from the record.
+// Return the sum of all the scores on the record after applying all the operations.
+
+// The test cases are generated such that the answer and all intermediate
+// calculations fit in a 32-bit integer and that all operations are valid.
+
+// Example 1:
+
+// Input: ops = ["5","2","C","D","+"]
+// Output: 30
+// Explanation:
+// "5" - Add 5 to the record, record is now [5].
+// "2" - Add 2 to the record, record is now [5, 2].
+// "C" - Invalidate and remove the previous score, record is now [5].
+// "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+// "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+// The total sum is 5 + 10 + 15 = 30.
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function (operations) {
+  const records = [];
+
+  for (let i = 0; i < operations.length; i++) {
+    if (!isNaN(operations[i])) {
+      records.push(+operations[i]);
+    } else if (isNaN(operations[i]) && operations[i] === "C") {
+      records.pop();
+    } else if (isNaN(operations[i]) && operations[i] === "D") {
+      const addDouble = records.at(-1) * 2;
+      records.push(addDouble);
+    } else if (operations[i] === "+") {
+      const sumOfPrev = records.at(-1) + records.at(-2);
+      records.push(sumOfPrev);
+    }
+  }
+
+  return records.reduce((a, b) => a + b, 0);
+};
