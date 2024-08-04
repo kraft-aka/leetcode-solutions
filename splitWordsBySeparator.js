15f6c6a6b54401f7953b2d1a6651dd1a44b1b184
kubat <kubat.aiupov@gmail.com>
2024-08-04 22:15:52 +0200

    add splitWordsBySeparator

---
+++ splitWordsBySeparator.js
@@ -0,0 +1,33 @@
+// Given an array of strings words and a character separator, split each string in words by separator.
+
+// Return an array of strings containing the new strings formed after the splits, excluding empty strings.
+
+// Notes
+
+// separator is used to determine where the split should occur, but it is not included as part of the resulting strings.
+// A split may result in more than two strings.
+// The resulting strings must maintain the same order as they were initially given.
+
+// Example 1:
+
+// Input: words = ["one.two.three","four.five","six"], separator = "."
+// Output: ["one","two","three","four","five","six"]
+// Explanation: In this example we split as follows:
+
+// "one.two.three" splits into "one", "two", "three"
+// "four.five" splits into "four", "five"
+// "six" splits into "six"
+
+// Hence, the resulting array is ["one","two","three","four","five","six"].
+
+/**
+ * @param {string[]} words
+ * @param {character} separator
+ * @return {string[]}
+ */
+var splitWordsBySeparator = function (words, separator) {
+  return words
+    .map((w) => w.split(separator))
+    .flat()
+    .filter((i) => i);
+};
