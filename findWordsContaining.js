287451962b2dd7899e355da60412aed7cf3b2963
kubat <kubat.aiupov@gmail.com>
2024-08-04 22:31:22 +0200

    add findWordsContaining

---
+++ findWordsContaining.js
@@ -0,0 +1,17 @@
+// 2942. Find Words Containing Character
+
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+  const res = [];
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) {
+      res.push(i);
+    }
+  }
+  return res;
+};
