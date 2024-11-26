0f4cf0aee6cd50f826faff7c89bcd35b0449f08a
kubat <kubat.aiupov@gmail.com>
2024-11-26 22:21:16 +0100

    add findDisappearedNumbers

---
+++ findDisappearedNumbers.js
@@ -0,0 +1,27 @@
+// 448. Find All Numbers Disappeared in an Array
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  let missingNums = [];
+  let numSet = new Set(nums);
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (!numSet.has(i)) {
+      missingNums.push(i);
+    }
+  }
+  return missingNums;
+};
