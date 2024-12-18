5e8d01dc298f24bf06af12705d035d6ff799f26c
kubat <kubat.aiupov@gmail.com>
2024-12-18 22:04:20 +0100

    add reverseString

---
+++ reverseString.js
@@ -0,0 +1,16 @@
+Code;
+
+// 344. Reverse String
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  return s.reverse();
+};
